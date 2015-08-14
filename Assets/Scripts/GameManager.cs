5ab5ac56cc96c5c2078345995cf39f72152248e2
Ozzyboshi <gun101@email.it>
2015-08-14 22:38:23 +0200

    new exit prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,12 +25,6 @@ public class GameManager : MonoBehaviour {
 
         boardScript = GetComponent<BoardManager>();
         InitGame();
-
-        //foreach (GameObject car in carInstances)
-        //{
-           // CarMovement movement = car.GetComponent<CarMovement>();
-            //movement.StartStraightMovement();
-        //}
 	}
 
     void InitGame()
@@ -38,22 +32,4 @@ public class GameManager : MonoBehaviour {
         boardScript.SetupScene(level);
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //if (carInstances!=null)
-        //{
-            //foreach  (GameObject car in carInstances) {
-                //Transform carposition = car.transform;
-                //carposition.Translate(3.5F * Time.deltaTime, 0, 0);
-                //LeanTween.moveX(car, 1f, 1f).setEase(LeanTweenType.easeInQuad).setDelay(1f);
-                //Vector3 currentPosition = car.transform.position;
-                //Vector3 endPosition = car.transform.position;
-                //LeanTween.move(car, new Vector3 [] { new Vector3(0, 0,0), new Vector3(1,0,0), new Vector3(1, 0,0), new Vector3(1,1,0) }, 1.0F).setEase(LeanTweenType.easeInQuad).setOrientToPath2d(true);
-              //  break;
-
-            //}
-            
-        //}
-	}
 }
